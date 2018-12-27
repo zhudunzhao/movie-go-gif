b9e37132d61d889f4c4e9fbe9d53e31be9258ed0
zhudu <zhudu@fonwi-zhu>
2018-12-27 18:07:09 +0800

    上传 Signed-off-by:zhudu <zhudu@fonwi-zhu>

---
+++ src/test.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "zhudu"
+__date__ = "$2018-12-27 18:06:50$"
+
+if __name__ == "__main__":
+    print "Hello World"
