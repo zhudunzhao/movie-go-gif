19f56bf26d7571ed17637999c5669ef3aec3335f
zhudunzhao <zhudunzhao@126.com>
2019-01-03 00:27:12 +0800

    1

--- src/widgets.py
+++ src/widgets.py
@@ -2,7 +2,12 @@
 # _*_ coding:utf-8 _*_
 
 
-import Tkinter
+import Tkinter as tk
+# 弹出
+import tkMessageBox as mb
+import tkFileDialog 
+import re #正则
+
 
 class GIF(object):
 
@@ -13,24 +18,36 @@ class GIF(object):
         self.height=height
         self.fps=fps
 
-    def mainWidget(self):
-        #窗体
-        root = Tkinter.Tk()
+        # mb.showinfo("welcome","Welcome Message")
+
+    # 窗体
+    def widget(self):
+        root = tk.Tk()
         root.title('Movie to GIF')#title
         root.geometry('480x200')#widget size
         root.resizable(width=False, height=True) 
-        #排版
-        Tkinter.Label(root,text='选择文件').grid(row=0)
+        return root
 
-        Tkinter.Label(root,text='转换类型').grid(row=1)
+    # 布局
+    def main(self):
+        root=self.widget()
+        #第一行
+        tk.Label(root,text='选择文件').grid(row=0)
+        Button(root, text = "路径选择", command = self.selectPath).grid(row = 0, column = 2,sticky=E+W)
+        #第二行
+        tk.Label(root,text='转换类型').grid(row=1)
 
         #主体循环
         root.mainloop()
+
+    #选择文件
+    def selectPath():
+        path_ = tkFileDialog.askopenfilename(filetypes=[("bmp格式".decode('gbk'),"py")])
 
     def toGif():
         return 
 
 if __name__ == "__main__":
     g=GIF()
-    g.mainWidget()
+    g.main()
     
