4f240407a0bbcab2288b5dcb8cc9145ba070d6d7
zhudunzhao <zhudunzhao@126.com>
2019-01-03 20:46:42 +0800

    new

--- src/widgets.py
+++ src/widgets.py
@@ -11,19 +11,15 @@ import re #正则
 
 class Application(tk.Frame):
 
-    def __init__(self,master=None,source='',target='',width=800,height=600,fps=60):
+    def __init__(self,master=None,source='',target='',height=600,width=800,fps=60):
         tk.Frame.__init__(self,master)
         # 参数
-        self.source=tk.StringVar()
-        self.target=tk.StringVar()
-        self.width=tk.StringVar()
-        self.height=tk.StringVar()
-        self.fps=tk.StringVar()
-        self.source.set(source)
-        self.target.set(target)
-        self.fps.set(fps)
-        self.width.set(width)
-        self.height.set(height)
+        self.source=source
+        self.target=target
+        self.width=width
+        self.height=height
+        self.fps=fps
+        
         # 显示窗口，并使用grid布局
         self.grid(row=0)
         # 创建控件
@@ -32,28 +28,44 @@ class Application(tk.Frame):
    
     # 布局
     def main(self):
+        # 初始化字段
+        source=tk.StringVar()
+        target=tk.StringVar()
+        width=tk.StringVar()
+        height=tk.StringVar()
+        fps=tk.StringVar()
+
         # 第一行(选择文件)
         tk.Label(self,text='选择文件:').grid(row=0)
-        tk.Entry(self,textvariable=self.source).grid(row=0,column=1,columnspan=2,sticky=tk.E+tk.W)
+        self.getsource=tk.Entry(self,textvariable=source).grid(row=0,column=1,columnspan=2,sticky=tk.E+tk.W)
         tk.Button(self, text = "路径选择", command = self.selectPath).grid(row = 0, column = 3,sticky=tk.E+tk.W)
         #第二行（高宽）
         tk.Label(self,text='尺寸:').grid(row=1)
-        tk.Entry(self,textvariable=self.width).grid(row=1,column=1,sticky=tk.E)
-        tk.Entry(self,textvariable=self.height).grid(row=1,column=2,sticky=tk.E)
+        tk.Entry(self,textvariable=width).grid(row=1,column=1,sticky=tk.E)
+        tk.Entry(self,textvariable=height).grid(row=1,column=2,sticky=tk.E)
         # 第三行（fps）
         tk.Label(self,text='fps:').grid(row=2)
-        tk.Entry(self,textvariable=self.fps).grid(row=2,column=1,columnspan=2,sticky=tk.E+tk.W)
+        tk.Entry(self,textvariable=fps).grid(row=2,column=1,columnspan=2,sticky=tk.E+tk.W)
         #第四行(保存路径)
         tk.Label(self,text='保存路径:').grid(row=3)
-        tk.Entry(self,textvariable=self.target).grid(row=3,column=1,columnspan=2,sticky=tk.E+tk.W)
+        tk.Entry(self,textvariable=target).grid(row=3,column=1,columnspan=2,sticky=tk.E+tk.W)
         tk.Button(self, text = "路径选择", command = self.savePath).grid(row = 3, column = 3,sticky=tk.E+tk.W)
         #第四行(保存按钮)
         tk.Button(self, text = "路径选择", command = self.done).grid(row = 4,column=1, columnspan = 2,sticky=tk.E+tk.W)
 
+        #设置默认值
+        source.set(self.source)
+        target.set(self.target)
+        fps.set(self.fps)
+        width.set(self.width)
+        height.set(self.height)
+        print self.fps
+
         
     # 提交处理
     def done(self):
-        pass
+        source=self.getsource.get()
+        print source
 
     #选择文件
     def selectPath(self):
@@ -83,6 +95,8 @@ if __name__ == "__main__":
     app = Application()
     # 设置窗口标题为'Movie to GIF'
     app.master.title('Movie to GIF')
+    # 设置默认值
+    app.fps=1000
     # 主循环开始
     app.mainloop()
     
